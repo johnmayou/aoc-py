f30f22d500e6a453cbf9887568d353994ac55a61
John Mayou <john@johnmayou.com>
2025-06-14 18:57:25 -0500

    day 4 part 2

---
+++ 2015/04.2.py
@@ -0,0 +1,12 @@
+import hashlib
+
+input = 'yzbqklnj'
+guess = 1
+
+def md5(s: str) -> str:
+  return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+while not md5(input+str(guess)).startswith('000000'):
+  guess += 1
+
+print(guess)
